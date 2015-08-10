b367d3bf3c7148c8597a8defcd3ba76886df3873
SunShinewyf <2632807692@qq.com>
2015-08-10 22:47:52 +0800

    update the file

---
+++ Application/Admin/Conf/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    //'配置项'=>'配置值'
+        '__ROOT__' => '/Admin',
+        'URL_MODEL'          => '2', //URL模式
+		'__UPLOADS__' => '/Admin/Uploads',
+		'SHOW_PAGE_TRACE'=>true,//开启页面Trace
+        '__PUBLIC__' => '/Admin/Public',
+);
+?>
