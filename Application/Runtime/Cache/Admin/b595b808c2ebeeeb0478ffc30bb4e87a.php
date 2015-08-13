ca3d44c3646541d58626a3ef083d7804b86fd765
sunshinewyf <2632807692@qq.com>
2015-08-13 18:18:58 +0800

    update the file

--- Application/Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
+++ Application/Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
@@ -5,6 +5,8 @@
 	<meta charset="utf-8">
 	<title>后台首页</title>
 	<link href="/Admin/Public/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/Admin/Public/css/font-awesome.min.css" rel="stylesheet">
+	<link href="/Admin/Public/css/base.css" rel="stylesheet">
 	<link href="/Admin/Public/css/admin.css" rel="stylesheet">
 	<script src="/Admin/Public/js/jquery.min.js"></script>
 	<script src="/Admin/Public/js/bootstrap.min.js"></script>
@@ -15,16 +17,68 @@
            	   Admin System
            </div>
 
-           <ul class="nav nav-right">
+           <ul class="nav nav-right right">
                 <li><a href=""><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;首页</a></li>
                 <li><a href=""><span class="glyphicon glyphicon-cog"></span>&nbsp;&nbsp;修改密码</a></li>
                 <li><a href=""><span class="glyphicon glyphicon-off"></span>&nbsp;&nbsp;退出</a></li>
            </ul>
         </div>
-      	<!-- <div class="container-fluid-full">
+      	<div class="container-fluid-full">
 		<div class="row-fluid">
-		
+		   <div class="slider left">
+    <ul class="slide-menu">
+        <li><a href=""><i class="fa icon-home"></i>首页介绍</a></li>
+        <li>
+           <a href=""><i class="fa icon-picture"></i>首页大图管理<i class="arrow icon-double-angle-right"></i></a>
+           <ul class="child-menu">
+               <li><a href=""><i class="fa icon-angle-right"></i>添加图片</a></li>
+               <li><a href=""><i class="fa icon-angle-right"></i>修改图片</a></li>
+               <li><a href=""><i class="fa icon-angle-right"></i>删除图片</a></li>
+           </ul>
+        </li>
+        <li>
+          <a href=""><i class="fa icon-edit"></i>文章管理<i class="arrow icon-double-angle-right"></i></a>
+          <ul class="child-menu">
+              <li><a href=""><i class="fa icon-angle-right"></i>添加文章</a></li>
+              <li><a href=""><i class="fa icon-angle-right"></i>修改文章</a></li>
+              <li><a href=""><i class="fa icon-angle-right"></i>文章列表</a></li>
+          </ul>
+        </li>
+
+         <li>
+          <a href=""><i class="fa icon-edit"></i>用户管理<i class="arrow icon-double-angle-right"></i></a>
+          <ul class="child-menu">
+              <li><a href=""><i class="fa icon-angle-right"></i>添加用户</a></li>
+              <li><a href=""><i class="fa icon-angle-right"></i>修改用户</a></li>
+              <li><a href=""><i class="fa icon-angle-right"></i>文章列表</a></li>
+          </ul>
+        </li>
+
+        <li>
+          <a href=""><i class="fa icon-cogs"></i>系统管理<i class="arrow icon-double-angle-right"></i></a>
+          <ul class="child-menu">
+              <li><a href=""><i class="fa icon-angle-right"></i>添加文章</a></li>
+              <li><a href=""><i class="fa icon-angle-right"></i>修改文章</a></li>
+              <li><a href=""><i class="fa icon-angle-right"></i>文章列表</a></li>
+          </ul>
+        </li>
+
+
+    </ul>
+
+</div>
+		   <div class="content">
+                <div class="right-menu">
+                	<p class="left"><i class="fa1 icon-home"></i>首页<i class="fa icon-angle-right"></i>Message</p>
+                	<p class="right">2015-8-13 星期四</p>
+                </div>
+		   </div>
 		</div>
-		</div> -->
+		</div>
+		<div class="clearfix"></div>
+		<div class="footer">
+	<p><i class="fa icon-envelope"></i>2632807692@qq.com&nbsp;&nbsp;<a href="https://github.com/SunShinewyf"><i class="fa  icon-github"></i>github</a>&nbsp;&nbsp; © 2015 powered by sunshine</p>
+
+</div>
 	</body>
 </html>
